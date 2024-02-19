b5ace7bac9af951c6d1595d76de87ffff0e8990d
samfersosa <ssosam@unal.edu.co>
2024-02-19 09:07:45 -0500

    asiento 4

--- src/test/java/test/asiento.java
+++ src/test/java/test/asiento.java
@@ -4,7 +4,7 @@ public class asiento {
 	String color;
 	int precio;
 	int registro;
-}
+
 	public void cambiarColor(String color) {
 		if (color == "amarillo" || color == "verde" || color == "rojo" || color == "negro" || color == "blanco")
 			this.color = color;
