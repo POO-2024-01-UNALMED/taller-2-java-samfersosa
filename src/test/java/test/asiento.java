b9251c72cb5f8d29381860cf8b6c5a9ce2ae8705
samfersosa <ssosam@unal.edu.co>
2024-02-19 09:03:45 -0500

    asiento 3

--- src/test/java/test/asiento.java
+++ src/test/java/test/asiento.java
@@ -4,18 +4,8 @@ public class asiento {
 	String color;
 	int precio;
 	int registro;
-	
-	public asiento (String color, int precio, int registro) {
-		this.color = color;
-		this.precio = precio;
-		this.registro = registro;
-		}
-	public void cambiarColor(String nuevoColor) {
-       
-        String[] coloresPermitidos = {"blanco", "amarillo", "negro", "verde", "rojo"};
-        
-    
-        for (String colorPermitido : coloresPermitidos) {
-            if (colorPermitido.equalsIgnoreCase(nuevoColor)) {
-                this.color = nuevoColor;
-            	}	
+}
+	public void cambiarColor(String color) {
+		if (color == "amarillo" || color == "verde" || color == "rojo" || color == "negro" || color == "blanco")
+			this.color = color;
+	}
