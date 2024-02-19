1f14310c666d029b58f58b64b5934744dcc3290e
samfersosa <ssosam@unal.edu.co>
2024-02-19 00:24:34 -0500

    asiento java 2

--- src/test/java/test/asiento.java
+++ src/test/java/test/asiento.java
@@ -11,12 +11,11 @@ public class asiento {
 		this.registro = registro;
 		}
 	public void cambiarColor(String nuevoColor) {
-        // Lista de colores permitidos
+       
         String[] coloresPermitidos = {"blanco", "amarillo", "negro", "verde", "rojo"};
         
-        // Verificar si el nuevo color está en la lista de colores permitidos
+    
         for (String colorPermitido : coloresPermitidos) {
             if (colorPermitido.equalsIgnoreCase(nuevoColor)) {
                 this.color = nuevoColor;
-                System.out.println("El color del asiento ha sido cambiado a " + nuevoColor + ".");
-                return;
+            	}	
