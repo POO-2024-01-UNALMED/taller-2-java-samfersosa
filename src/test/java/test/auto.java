762c01a46106b687cd54de817753ed45a65c0dc5
samfersosa <ssosam@unal.edu.co>
2024-02-19 09:50:06 -0500

    Auto

---
+++ src/test/java/test/auto.java
@@ -0,0 +1,25 @@
+package test;
+
+public class auto {
+String modelo;
+int precio;
+Asiento [] asientos;
+String marca;
+Motor motor;
+int registro;
+int cantidadCreados;
+int contador = 0;
+	public void cantidadAsientos(int asientos) {
+		
+		for (asiento asiento : asientos) {
+	            if (asiento != null) { 
+	                contador++;
+	                System.out.println(contador);
+	}
+	public void verificarIntegridad(String Motor, Auto, Asiento) {
+		if (motor == Auto == asientos)
+			System.out.println("Auto original");
+		else 
+			System.out.println("Las piezas no son originales");
+	}
+}}}
